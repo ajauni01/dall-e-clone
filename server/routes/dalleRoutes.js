8cd0b611a94b8476f2a5a077dcc5cbea42743453
Ajharul Islam Aunik <ajauni01@wsc.edu>
2023-10-02 02:40:18 -0500

    OpenAI typo updated

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import openai from "openai/index.js";
+import OpenAI from "openai/index.js";
 // populate the environental variables
 dotenv.config();
 // create an express framework instance
