f5f5e8ba5037db38c425bc04241508ccf9a1f931
AJHARUL ISLAM AUNIK <aunik@AJHARULs-Air.home>
2023-12-20 23:47:58 -0600

    Update the client side

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import { downloadImage } from "../../utils/index.js";
 import "./Card.css";
 
 const Card = ({ _id, name, prompt, photo }) => {
-  // console.log("prompts in the card", prompt);
+  // console.log("name", typeof name);
   return (
     <div
       className="rounded-xl group 
@@ -19,12 +19,14 @@ const Card = ({ _id, name, prompt, photo }) => {
         className="max-h-[94.5%] absolute bottom-0 left-0
        right-0 bg-[#10131f] m-2 p-4 rounded-md prompt"
       >
+        {/* show the prompt to the user */}
         <p className="text-white text-md overflow-y-auto">{prompt}</p>
 
         <div className="mt-5 flex justify-between items-center gap-2">
           <div className="flex items-center gap-2">
             <div className="w-7 h-7 rounded-full object-cover bg-green-700 flex justify-center items-center text-white text-xs font-bold">
-              {name[0]}
+              {/* show the first alphabet of the user name above the picture only when name exists */}
+              {name && name[0]}
             </div>
             <div>
               <p className="text-white text-sm">{name}</p>
@@ -45,4 +47,3 @@ const Card = ({ _id, name, prompt, photo }) => {
 };
 
 export default Card;
-
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
     const fetchPosts = async () => {
       try {
         const response = await fetch(
-          "https://dall-e-clone-server-mj3kzml2d-ajauni01.vercel.app/api/v1/post",
+          "http://localhost:3000/api/v1/post",
           {
             method: "GET",
             headers: {
