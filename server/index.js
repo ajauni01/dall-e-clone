02d9e006fa47badd1826c470ace6153aa7d787a9
AJHARUL ISLAM AUNIK <aunik@AJHARULs-Air.home>
2023-12-20 23:00:39 -0600

    Fix the server issues

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-import express from "express";
-import * as dotenv from "dotenv";
 import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
 import connectDB from "./mongodb/connect.js";
-import postRoutes from "./routes/postRoutes.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
 
 // load environment variables
 dotenv.config();
@@ -29,13 +29,12 @@ const startServer = async () => {
   // connect to mongoDB
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(5000, () =>
-      console.log("Server is running on port http://localhost:5000")
+    app.listen(3000, () =>
+      console.log("Server is running on port http://localhost:3000")
     );
   } catch (error) {
     console.log(error);
   }
 };
+// call the startServer function
 startServer();
-
-
