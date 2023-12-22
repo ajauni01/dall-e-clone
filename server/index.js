7a456c77ac2aac8ad52a6f23a285895f749b1ff1
AJHARUL ISLAM AUNIK <aunik@AJHARULs-Air.home>
2023-12-22 00:09:56 -0600

    Update the vercel.json

--- server/index.js
+++ server/index.js
@@ -1,21 +1,20 @@
+import cors from "cors";
 import * as dotenv from "dotenv";
 import express from "express";
 import connectDB from "./mongodb/connect.js";
 import dalleRoutes from "./routes/dalleRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
-import cors from "cors";
 
 // Load environment variables
 dotenv.config();
 
 // Create an instance of the express application
 const app = express();
+// configure cors
+app.use(cors());
 
 // Configure express to parse JSON requests
 app.use(express.json({ limit: "50mb" }));
-
-// configure cors
-app.use(cors());
 
 // API routes
 app.use("/api/v1/post", postRoutes);
@@ -26,14 +25,11 @@ app.get("/", async (req, res) => {
   res.send("Hello from DALL-E Cloneaazzzzaaaa");
 });
 
-// Handle OPTIONS requests
-app.options("*", cors()); // Respond to preflight requests
-
 // Connect to MongoDB
 connectDB(process.env.MONGODB_URL);
 
 // Start the server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
